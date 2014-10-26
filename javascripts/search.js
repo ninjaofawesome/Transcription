dbf7d95b6104871e440f04378579cf1c41f2e1b8
Hannah Nordgren <hey@hereshannahs.info>
2014-10-26 15:23:44 -0400

    finished

--- javascripts/search.js
+++ javascripts/search.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
   console.log('search option');
 
-  $( ".clickme" ).click(function() {
-    $( ".search-options" ).slideToggle( "slow", function() {});
+  $( ".search-button" ).click(function() {
+    console.log('click');
+    $( ".search-grid" ).slideToggle( "slow", function() {
+        $(this).css('display', 'inline');
+    });
   });
 
 });
--- javascripts/transcription.js
+++ javascripts/transcription.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
 
   var params = { allowScriptAccess: "always" };
   var atts = { id: "myytplayer" };
-  //not sure what swfobject does, you need to define it.  Maybe canvas is a better option than a flash player?
-  swfobject.embedSWF("http://www.youtube.com/v/5_0dVHMpJlo?enablejsapi=1&playerapiid=ytplayer",
-                     "ytapiplayer", "425", "356", "8", null, null, params, atts);
+  //not sure what swfobject does, you need to define it. Canvas is a better option than a flash player?
+  // swfobject.embedSWF("http://www.youtube.com/v/5_0dVHMpJlo?enablejsapi=1&playerapiid=ytplayer",
+  //                    "ytapiplayer", "425", "356", "8", null, null, params, atts);
 
   function onYouTubePlayerReady(playerId) {
     ytplayer = document.getElementById("myytplayer");
@@ -17,7 +17,6 @@ $(document).ready(function(){
   }
   return false;
   }
-
 
 });
 
